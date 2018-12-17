e8c6602bdc2b91bb33241b3c70cf7bcf1485f463
dragonhht <dragon_hht@163.com>
2018-12-17 23:46:00 +0800

    基础程序

---
+++ simple-web/src/main/java/com/github/dragonhht/web/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.github.dragonhht.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 描述.
+ *
+ * @author: huang
+ * @Date: 18-12-17
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
